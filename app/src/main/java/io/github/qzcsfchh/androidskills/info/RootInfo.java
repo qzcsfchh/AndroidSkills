2bdf9c9f6f5c3f451bb57ebbde5bb58513d9176d
qzscfchh <csfchh@qq.com>
2021-07-22 23:42:04 +0800

    Add RootInfo.

---
+++ app/src/main/java/io/github/qzcsfchh/androidskills/info/RootInfo.java
@@ -0,0 +1,31 @@
+package io.github.qzcsfchh.androidskills.info;
+
+import java.io.File;
+
+/**
+ * Desc.
+ *
+ * @author huanghao
+ * @version v1.0
+ * @since 2021/7/21 22:28
+ * @see <a href='https://juejin.cn/post/6987183668968030221?utm_source=gold_browser_extension'>一文带你了解android检测root方案</a>
+ */
+public class RootInfo {
+    private RootInfo() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean c() {
+        String[] paths = {"/system/bin/", "/system/xbin/", "/system/sbin/", "/sbin/", "/vendor/bin/","/su/bin/"};
+        for (String path : paths) {
+            try {
+                if (new File(path + "su").exists()) {
+                    return true;
+                }
+            } catch (Exception ignore) { }
+        }
+        return false;
+    }
+
+
+}
